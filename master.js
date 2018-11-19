309e845f65ee62bf58d91fca675e8bd855d5c766
arioncy <arioncy8@gmail.com>
2018-11-19 21:07:15 +0700

    Update master.js

--- master.js
+++ master.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
 if(x=="1"){xxx="wortel-sayuran-dengan-sejuta-manfaat.html"}
 if(x=="2"){xxx="a.html"}a_to_ve[i].href="https://hidup-sehat-dan-alami.blogspot.com/2018/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"n";}}
