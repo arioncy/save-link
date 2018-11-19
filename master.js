e96ea3eb98ebe1c78d26ef34a72abfc20bb28113
arioncy <arioncy8@gmail.com>
2018-11-19 19:13:12 +0700

    Update master.js

--- master.js
+++ master.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="wortel-sayuran-dengan-sejuta-manfaat.html"}
-if(x=="2"){xxx="a.html"}a_to_ve[i].href="https://safelink-movie.blogspot.com/2018/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"n";}}
+if(x=="2"){xxx="a.html"}a_to_ve[i].href="https://hidup-sehat-dan-alami.blogspot.com/2018/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
